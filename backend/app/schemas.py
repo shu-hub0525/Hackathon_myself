b570cab33609a4d6ed3dc7e9558cfc412654d4a4
shu-hub0525 <shugo.y0525@gmail.com>
2025-07-19 16:54:16 +0900

    バックエンド実装-API連携

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,19 +1,35 @@
 from pydantic import BaseModel
 from typing import Optional
 
+
 class UserBase(BaseModel):
     name: str
     age: int
 
+
 class UserCreate(UserBase):
     pass
+
 
 class UserUpdate(BaseModel):
     name: Optional[str] = None
     age: Optional[int] = None
+
 
 class User(UserBase):
     id: int
 
     class Config:
         from_attributes = True
+
+
+# フロントエンドから受け取るリクエストの型
+class TranslationRequest(BaseModel):
+    text: str
+    dialect: str
+    direction: str
+
+
+# フロントエンドへ返すレスポンスの型
+class TranslationResponse(BaseModel):
+    translated_text: str
